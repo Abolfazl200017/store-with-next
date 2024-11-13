b67cbedb2abd1d9698b9efda154b0c49373e7255
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-11-13 15:06:02 +0330

    config header view template

--- src/components/layout/Header/HeaderContainer.tsx
+++ src/components/layout/Header/HeaderContainer.tsx
@@ -1,35 +1,91 @@
-import { siteConfig } from '@/constants';
-import { Box, Typography } from '@mui/material';
-import React from 'react';
+import { siteConfig } from "@/constants";
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import Link from "next/link";
+import LoginIcon from "@mui/icons-material/Login";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function LogoName() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Box component="img" src="/images/logo.webp" sx={{ height: 50 }} />
+      <Typography variant="h1">{siteConfig.name}</Typography>
+    </Box>
+  );
+}
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            alignItems: 'center',
-            paddingTop: 3,
-            paddingButton: 3,
-        }}>
-            <Box component='img' src='/images/logo.webp' sx={{ height: 50 }} />
-            <Typography variant='h1'>
-                {siteConfig.name}
-            </Typography>
+function LinkList() {
+  const Links = [
+    { name: "خانه", href: "/" },
+    { name: "محصولات", href: "/products" },
+    { name: "درباره ما", href: "/about" },
+  ];
+
+  return (
+    <Box component="ul" sx={{ display: "flex", alignItems: "center" }}>
+      {Links.map((link) => (
+        <Link href={link.href} key={link.href}>
+          <Typography sx={{ fontWeight: "bold", paddingX: 1, color: "black" }}>
+            {link.name}
+          </Typography>
+        </Link>
+      ))}
+    </Box>
+  );
+}
+
+function LoginOrBasket() {
+  const isLoggedIn = false;
+
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      {isLoggedIn ? (
+        <Box>
+          <ShoppingBasketIcon />
         </Box>
-    )
+      ) : (
+        <Link href='/register'>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography sx={{ ml: 1 }}>ورود</Typography>
+            <LoginIcon />
+            </Box>
+        </Link>
+      )}
+    </Box>
+  );
 }
 
 export const HeaderContainer: React.FC = () => {
   return (
     <header>
-        <Box sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-        }}>
-            <LogoName />
-
+      <Box
+        sx={{
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            width: 1,
+            maxWidth: "1280px",
+            paddingX: 3,
+            paddingY: 2,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <LogoName />
+          <LinkList />
+          <LoginOrBasket />
         </Box>
+      </Box>
     </header>
   );
 };
--- src/components/layout/LayoutContainer.tsx
+++ src/components/layout/LayoutContainer.tsx
@@ -11,15 +11,8 @@ export const LayoutContainer: React.FC<LayoutProps> = ({ children }) => {
   return (
     <Box
       sx={{
-        display: "grid",
-        gridTemplateRows: "20px 1fr 20px",
-        alignItems: "center",
-        justifyItems: "center",
-        minHeight: "100svh",
         padding: 0,
         margin: 0,
-        gap: "64px",
-        paddingTop: '15px',
       }}
     >
       <Header />
