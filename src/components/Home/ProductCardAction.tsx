8069f41355517dd232b645a9b1d1d8743395d2b4
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-11-16 12:55:36 +0330

    config increase decrease action on product card list

--- src/components/Home/ProductCardAction.tsx
+++ src/components/Home/ProductCardAction.tsx
@@ -2,10 +2,11 @@
 
 import { Product } from "@/services/api/types";
 import useBasketStore from "@/store/basketStore";
+import { theme } from "@/theme/theme";
 import { Box, Button, Typography } from "@mui/material";
 
 const ProductCardAction = ({ product }: { product: Product }) => {
-  const { products, addProduct } = useBasketStore();
+  const { products, addProduct, updateProductQuantity, removeProduct } = useBasketStore();
 
   const isProductInBasket = products.some((p) => p.id === product.id);
   const handleAddToBasket = () => {
@@ -13,22 +14,70 @@ const ProductCardAction = ({ product }: { product: Product }) => {
       addProduct(product, 1);
     }
   };
+  const getQuantity = () => {
+    const index: number = products.findIndex((p) => p.id === product.id);
+    if (index === -1)
+      return 0;
+    else return products[index].quantity
+  }
+
+  const increaseQuantity = () => {
+    const quantity:number = getQuantity();
+    updateProductQuantity(product.id.toString(), quantity+1);
+  }
+
+  const decreaseQuantity = () => {
+    const quantity:number = getQuantity();
+    if (quantity <= 1)
+      removeProduct(product.id.toString())
+    else
+      updateProductQuantity(product.id.toString(), quantity-1);
+  }
 
   return (
     <Box>
       {isProductInBasket ? (
-        <Typography
+        <Box
           sx={{
-            backgroundColor: "white",
-            borderRadius: 0,
-            px: 3,
-            py: 1,
-            fontSize: '0.9rem',
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            border: 1,
+            borderColor: theme.palette.primary.main,
+            backgroundColor: theme.palette.secondary.main,
+            borderRadius: 3,
+            color: "black",
+            height: "40px",
           }}
-          className="text-primary"
         >
-          موجود در سبد
-        </Typography>
+          <Button
+            variant="text"
+            sx={{
+              color: "black",
+              fontSize: "1.5rem",
+              height: 1,
+              ":hover": { backgroundColor: "transparent" },
+            }}
+            onClick={decreaseQuantity}
+          >
+            -
+          </Button>
+          <Typography sx={{ px: 1 }}>
+            {getQuantity()}
+          </Typography>
+          <Button
+            variant="text"
+            sx={{
+              color: "black",
+              fontSize: "1.1rem",
+              height: 1,
+              ":hover": { backgroundColor: "transparent" },
+            }}
+            onClick={increaseQuantity}
+          >
+            +
+          </Button>
+        </Box>
       ) : (
         <Button
           variant="text"
